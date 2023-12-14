9d628d2a33b4c74223cf9feb2dff991948b4f934
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2023-12-14 12:16:47 +0500

    Add files via upload

---
+++ 05. Conditional Statements/5.9.py
@@ -0,0 +1,18 @@
+#Take a day number and display day name.
+day = int(input("Enter Day Number "))
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("Invalid Number")
