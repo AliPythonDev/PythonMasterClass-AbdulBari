c3ecee32fa6ee046271f4cb5abe1ee292d047115
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2023-12-15 11:10:40 +0500

    Add files via upload

---
+++ 06. Loops - Control Flow/06->11.py
@@ -0,0 +1,17 @@
+#For Loop
+#a)
+# msg = 'Hello'
+# for x in msg:
+#     print(x)
+#b)
+for i in range(10): #Similar for i in range(0 , 10):
+    print(i)
+#for i in range(5 , 10):        Output: 5 6 7 8 9
+#for i in range(1 , 10 , 2):    Output: 1 3 5 7
+#for i in range(0 , 10 , 1):    Output: 0 1 2 3 4 5 6 7 8 9
+#for i in range(10 , 0 , -1):   Output: 10 9 8 7 6 5 4 3 2 1
+#for i in range(-10 , -1 , 2):   Output: -10 -8 -6 -4 -2
+#for i in range(-10 , -2 , 2):   Output: -10 -8 -6 -4
+# 10 is Starting Point of Range, 0 is Ending Point of Range and -1 is Step
+# 0 is not printed so you can treat it as False.
+# For each i in Range (0 , 10) it will take values from 0-9
