23b14648148d3ad1aeadf2af2071c2893e81e612
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2023-12-15 11:12:41 +0500

    Add files via upload

---
+++ 06. Loops - Control Flow/06->15.py
@@ -0,0 +1,27 @@
+#Brake Statement
+#a)
+# for i in range (0, 10):
+#     print(i)
+# else:
+#     print("For Completed Properly")
+
+#b)
+# for i in range (0, 10):
+#     if i > 5:
+#         break
+#     else:
+#         print(i)
+# else:
+#     print("For Completed Properly")
+
+#Pass
+# for i in range (0,10):
+#     pass
+# print("Program Ended.")
+
+#Continue Statement
+for i in range (0, 10):
+    if i % 5 == 0:
+        continue
+    print(i)
+#If the condition is True it will skip printing and if it is False then it will keep printing
