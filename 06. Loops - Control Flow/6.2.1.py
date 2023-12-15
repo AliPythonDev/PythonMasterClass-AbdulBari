73c7a807c81c4b7386360ffb242543771d043373
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2023-12-15 10:41:57 +0500

    Delete 06. Loops - Control Flow directory

--- 06. Loops - Control Flow/6.2.1.py
+++
@@ -1,6 +0,0 @@
-#Q) Print hello 10 times using while loop.
-#In while loop use the counter as condition and set its value from 1 -10 , modify the counter In the statements and get the result.
-count = 1
-while count <= 10:
-    print(count, "Hey")
-    count = count + 1
--- 06. Loops - Control Flow/6.3.py
+++
@@ -1,7 +0,0 @@
-#Q) Display multiplication table for a given number
-#We have a number and for that we have to print a multiplication table, as the pattern for multiplying is same we are using while loop here.
-n = int(input("Enter a Number which you want to print a Table: "))
-counter = 1
-while counter <= 10:
-    print(counter, " X ", n, " = ", counter * n)
-    counter += 1
